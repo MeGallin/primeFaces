2961206cb6bfcec6d51c7b4a2b377412368322d1
MeGallin <me@garyallin.uk>
2017-04-30 17:52:55 +0100

    Working Charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import {AccordionModule, ChartModule} from 'primeng/primeng';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccordionModule, ChartModule]
 })
 export class AppComponent {
-  title = 'app works!';
+  data: any;
+
+  msgs: any[];
+
+  constructor() {
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#4bc0c0'
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#565656'
+        }
+      ]
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {AccordionModule} from 'primeng/components/accordion/accordion';
+import { ChartModule } from 'primeng/components/chart/chart'
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +16,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    AccordionModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
